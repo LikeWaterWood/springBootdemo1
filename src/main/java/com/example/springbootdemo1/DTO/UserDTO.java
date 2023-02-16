896caf49cf74844b6ac1c39fe91cc5a1dba8b4aa
YingJie Lu (鲁英杰)-浪潮软件 <luyingjie01@inspur.com>
2023-02-16 11:08:49 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootdemo1/DTO/UserDTO.java
@@ -0,0 +1,25 @@
+package com.example.springbootdemo1.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+/**
+ * @Description: 用户实体类
+ **/
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String loginName;
+    private String password;
+    private String userName;
+    private String sex;
+    private String email;
+    private String address;
+}
